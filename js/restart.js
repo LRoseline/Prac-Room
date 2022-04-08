7a3c358b80458981c08ff9c20d45bf202003bddd
LRoseline <amb17@naver.com>
2022-04-08 16:37:03 +0900

    Moved

--- js/restart.js
+++ js/restart.js
@@ -1,4 +1,31 @@
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('test')
+const a = "Hello";
+const listarray = (
+    <div>
+        <li>{a}</li>
+        <li>{a}</li>
+        <li>{a}</li>
+    </div>
 );
+
+const renderFor = (num) => {
+    const result = [];
+
+    for (let i = 1; i <= num; i++) {
+        result.push(<li>{i}</li>);
+    }
+
+    return result;
+}
+
+const renderResult = (
+    <div>
+        {renderFor(10)}
+    </div>
+);
+
+const testA = document.getElementById('testA');
+const testB = document.getElementById('testB');
+const testC = document.getElementById('testC');
+ReactDOM.render(<h1>Hello, world!</h1>, testA);
+ReactDOM.render(listarray, testB);
+ReactDOM.render(renderResult, testC);
