804548de5cd0bf43453eaa767ff5b34da830a423
LRoseline <amb17@naver.com>
2022-04-21 16:11:47 +0900

    maptest

--- js/maps.js
+++ js/maps.js
@@ -1,18 +1,20 @@
 const xy = [37.55411, 126.92010];
 const nobrand = "노브랜드 버거 홍대점";
+const optionmap = {
+    center: [37.55411, 126.92010],
+    zoom: 18
+}
 
-var map = L.map('map').setView(xy, 18);
+const map = new L.map('map').setView(xy, 18);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var layer = new L.TileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+});
 
-L.marker(xy).addTo(map)
-    .bindPopup('<b>서교동</b>')
-    .openPopup();
+map.addLayer(layer);
 
-L.marker([37.5544898, 126.9221304]).addTo(map)
-    .bindPopup(nobrand)
-    .on('click', function (e) {
-        alert(nobrand);
-    });
+var marker1 = L.marker(xy).addTo(map);
+var marker2 = L.marker([37.5544898, 126.9221304]);
+marker2.on("click",function () {
+    alert(nobrand);
+}).addTo(map);;
